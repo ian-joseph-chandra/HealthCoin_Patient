340deabdfd0ab8029e88c2a4a1deb9f215ed2e5f
laurenshendrika <60267241+laurenshendrika@users.noreply.github.com>
2020-02-06 17:33:17 +0700

    2020/02/06 Login Page
    1. Optimize getScreenSize() Function.
    2. Remove registration component.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { HostListener } from '@angular/core';
+import {HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -27,8 +27,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  @HostListener('window:resize', ['$event'])
-  getScreenSize(event?) {
+  getScreenSize() {
     this.screenHeight = window.innerHeight;
     this.screenWidth = window.innerWidth;
   }
@@ -40,17 +39,17 @@ export class LoginComponent implements OnInit {
     this.inputPass = document.getElementById('password').value;
 
     console.log('username: ' + this.inputUserOrEmail + '\nPassword: ' + this.inputPass);
-}
-
-login() {
-  this.getInputUser();
-
-  if ((this.inputUserOrEmail === this.usernameDatabase || this.inputUserOrEmail === this.emailDatabase)
-    && this.inputPass === this.passwordDatabase) {
-    console.log('Login Success!');
-  } else {
-    console.log('Login failed!');
   }
 
-}
+  login() {
+    this.getInputUser();
+
+    if ((this.inputUserOrEmail === this.usernameDatabase || this.inputUserOrEmail === this.emailDatabase)
+      && this.inputPass === this.passwordDatabase) {
+      console.log('Login Success!');
+    } else {
+      console.log('Login failed!');
+    }
+
+  }
 }
