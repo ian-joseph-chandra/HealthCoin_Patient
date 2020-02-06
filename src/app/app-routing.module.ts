7a6637b61e301a628d15679ff9fb53ad80e32c02
Evelyn <evelyn82@ui.ac.id>
2020-02-06 09:26:50 +0700

    Revert "Generate pages/login"
    This reverts commit 35aa86152025f0b89d29a1b74b8406eb6f92b448, reversing
    changes made to c2b67a75351195a4475c2a590d084d7be5fcdd9e.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {LoginComponent} from './pages/login/login.component';
 
 
-const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
