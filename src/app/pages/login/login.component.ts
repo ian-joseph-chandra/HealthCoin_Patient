620633839596bcea186b79c236e712af2156f73d
Evelyn <evelyn82@ui.ac.id>
2020-02-06 09:44:12 +0700

    Revert "Revert "Generate pages/login""
    This reverts commit 7a6637b61e301a628d15679ff9fb53ad80e32c02.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
+=======
+import {Component, OnInit} from '@angular/core';
+>>>>>>> dbca634423990aa5610ac950f2ac14d5a0640d4f
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+<<<<<<< HEAD
   constructor() { }
+=======
+  usernameDatabase = 'lauren';
+  emailDatabase = 'lauren@gmail.com';
+  passwordDatabase = 'lauren123';
+
+  inputUserOrEmail = '';
+  inputPass = '';
+
+  constructor() {
+  }
+>>>>>>> dbca634423990aa5610ac950f2ac14d5a0640d4f
 
   ngOnInit() {
   }
 
+<<<<<<< HEAD
+=======
+  getInputUser() {
+    // @ts-ignore
+    this.inputUserOrEmail = document.getElementById('username').value;
+    // @ts-ignore
+    this.inputPass = document.getElementById('password').value;
+
+    console.log('username: ' + this.inputUserOrEmail + '\nPassword: ' + this.inputPass);
+}
+
+login() {
+  this.getInputUser();
+
+  if ((this.inputUserOrEmail === this.usernameDatabase || this.inputUserOrEmail === this.emailDatabase)
+    && this.inputPass === this.passwordDatabase) {
+    console.log('Login Success!');
+  } else {
+    console.log('Login failed!');
+  }
+
+}
+>>>>>>> dbca634423990aa5610ac950f2ac14d5a0640d4f
 }
