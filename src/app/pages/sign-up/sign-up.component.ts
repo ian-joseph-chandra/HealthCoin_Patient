b2bb6eb4463f26473a4e3833a83b9cdb2f8a5649
ianjoseph2204 <44733223+ianjoseph2204@users.noreply.github.com>
2020-02-06 16:04:05 +0700

    2020/02/06 Registration Page
    create registration page like a google account

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  screenWidth: number;
+  screenHeight: number;
 
   constructor() { }
 
