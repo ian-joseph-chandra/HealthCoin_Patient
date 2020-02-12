8fa5fe6c1f0b112e0bd33d9ac9037281c7daf161
Evelyn <evelyn@ui.ac.id>
2020-02-12 17:46:59 +0700

    2020/02/12 Updating profile component
    Unfinished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { UserRecoveryComponent } from './pages/user-recovery/user-recovery.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { UserRecoveryComponent } from './pages/user-recovery/user-recovery.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
