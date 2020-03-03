5a8c1bb8d6f99ed2dff2027bfc161478754c0f5a
laurenshendrika <60267241+laurenshendrika@users.noreply.github.com>
2020-03-03 17:53:26 +0700

    2020/02/03
    Create looping record list

--- src/app/pages/record-list/record-list.component.ts
+++ src/app/pages/record-list/record-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordListComponent implements OnInit {
 
+  records: any = Array(10).fill(0);
   constructor() { }
 
   ngOnInit() {
