67bf2d5fbfcd093fcf6612603652315144ec470a
Evelyn247 <evelyn82@ui.ac.id>
2020-03-29 15:13:31 +0700

    Sending response to profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterService} from '../../services/router/router.service';
+import {ApiService} from '../../services/api/api.service';
+import {async} from '@angular/core/testing';
 
 @Component({
   selector: 'app-profile',
@@ -8,10 +10,44 @@ import {RouterService} from '../../services/router/router.service';
 })
 
 export class ProfileComponent implements OnInit {
+  inputUserId = '1';
 
-  constructor(public router: RouterService) {
+  requestSent = false;
+  userId: string;
+  error = false;
+  message: string;
+  name: string;
+
+  constructor(public router: RouterService, private api: ApiService) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.getProfile();
+  }
+
+  async getProfile() {
+    await this.sendProfileData();
+  }
+
+  async sendProfileData() {
+    // Set status for requestSent.
+    this.requestSent = true;
+
+    // Prepare the JSON data to be parsed to API Server
+    const profileQueryJSON = {
+      user_id: 1,
+    };
+
+    // Send the data to the API server & store the response.
+    const profileQueryResponse = await this.api.sendPostRequest('profile', profileQueryJSON);
+
+    this.error = profileQueryResponse.error;
+
+    if (this.error) {
+      this.message = profileQueryResponse.message;
+    } else {
+      this.name = profileQueryResponse.data.first_name + ' ' + profileQueryResponse.data.last_name;
+      console.log(profileQueryResponse.data)
+    }
   }
 }
