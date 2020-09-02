1818833486baf8cb033564ae40e5c2f1f6c88122
Joshfrederich <51415146+Joshfrederich@users.noreply.github.com>
2020-09-03 00:29:28 +0700

    Update QR Code page
    1. Create QR Code scan feature
    2. Test the QR Code feature in the HTML

--- src/app/pages/qr-code/qr-code.component.ts
+++ src/app/pages/qr-code/qr-code.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {RouterService} from '../../services/router/router.service';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-qr-code',
@@ -7,10 +8,113 @@ import {RouterService} from '../../services/router/router.service';
   styleUrls: ['./qr-code.component.css']
 })
 export class QrCodeComponent implements OnInit {
+  @ViewChild('video', {static: false}) video: ElementRef;
+  @ViewChild('canvas', {static: false}) canvas: ElementRef;
+  @ViewChild('fileinput', {static: false}) fileinput: ElementRef;
 
-  constructor(public router: RouterService) { }
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+
+  constructor(public router: RouterService) {
+  }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+  }
+
+  reset() {
+    this.scanResult = null;
+  }
+
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  async startScan() {
+    // Not working on iOS standalone mode!
+    this.videoElement.srcObject = await navigator.mediaDevices.getUserMedia({
+      video: {facingMode: 'environment'}
+    });
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      this.scanActive = true;
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        console.log(this.scanResult);
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+
+  handleFile(files: FileList) {
+    const file = files.item(0);
+
+    const img = new Image();
+
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanResult = code.data;
+        console.log(this.scanResult);
+      }
+    };
+    img.src = URL.createObjectURL(file);
+  }
 }
